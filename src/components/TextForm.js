562af627d4162d74f1766e875eb36d5ce1138b78
Amit Nandode <nandodeamit@gmail.com>
2023-06-07 23:39:53 +0530

    About component added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,26 +6,51 @@ export default function TextForm(props) {
     setText(newText);
   };
 
+  const handleLowerClick = () => {
+    const newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearClick = () => {
+    setText("");
+  };
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
 
   const [text, setText] = useState("Enter text here");
   return (
-    <div>
-      <h3>{props.heading}</h3>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="myBox"
-          value={text}
-          rows="7"
-          onChange={handleOnChange}
-        ></textarea>
+    <>
+      <div className="container">
+        <h3>{props.heading}</h3>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            value={text}
+            rows="7"
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpperClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLowerClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+          Clear
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpperClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container my-2">
+        <h1>Your text summary</h1>
+        <p>
+          {text.trim().split(" ").length} words and {text.length} chars
+        </p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
